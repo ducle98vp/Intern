8811640bd07d4cf602b345eb3fec94a174b2ba55
duclh <duclh@sudobo.com>
2021-01-04 12:57:57 +0700

    init project

---
+++ backend/app/Category.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\BaseModel;
+
+class Category extends BaseModel
+{
+    protected $table = 'categories';
+}
---
+++ backend/app/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\BaseModel;
+
+class Product extends BaseModel
+{
+    protected $table = 'products';
+}
